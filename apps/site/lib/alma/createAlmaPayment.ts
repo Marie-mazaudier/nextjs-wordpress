7e1702aba320c483efa775ee07c26ca02a52c2fb
Marie-mazaudier <mariemazaudier.dev@gmail.com>
2024-02-24 17:54:10 +0100

    ajout point sécurité appel api

--- apps/site/lib/alma/createAlmaPayment.ts
+++ apps/site/lib/alma/createAlmaPayment.ts
@@ -26,76 +26,7 @@ interface OrderData {
 }
 
 const createAlmaPayment = async (orderInfo: OrderData) => {
-    /* console.log("Alma Payload:", {
-         payment: {
-             purchase_amount: parseInt(orderInfo.total) * 100, // Convertit en centimes
-             installments_count: 3,
-             billing_address: {
-                 company: orderInfo.billing.company,
-                 first_name: orderInfo.billing.first_name,
-                 last_name: orderInfo.billing.last_name,
-                 email: orderInfo.billing.email,
-                 phone: orderInfo.billing.phone,
-                 line1: orderInfo.billing.address_1,
-                 line2: orderInfo.billing.address_2,
-                 postal_code: orderInfo.billing.postcode,
-                 city: orderInfo.billing.city,
-                 country: orderInfo.billing.country,
-             },
-             customer_cancel_url: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/checkout`,
-             custom_data: "Quelques données personnalisées",
-             deferred_months: 0,
-             deferred_days: 0,
-             ipn_callback_url: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/api/payments/validate-payment`,
-             origin: "online",
-             return_url: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}`,
-             shipping_address: {
-                 company: orderInfo.shipping.company,
-                 first_name: orderInfo.shipping.first_name,
-                 last_name: orderInfo.shipping.last_name,
-                 email: orderInfo.shipping.email,
-                 phone: orderInfo.shipping.phone,
-                 line1: orderInfo.shipping.address_1,
-                 line2: orderInfo.shipping.address_2,
-                 postal_code: orderInfo.shipping.postcode,
-                 city: orderInfo.shipping.city,
-                 country: orderInfo.shipping.country,
-             },
-             locale: "fr",
-             expires_after: 2880,
-             capture_method: "automatic",
-             failure_return_url: `${process.env.NEXT_PUBLIC_PRODUCTION_URL}/failure`,
-         },
-         customer: {
-             id: orderInfo.customer_id, // 
-             created: orderInfo.date_created,
-             first_name: orderInfo.billing.first_name,
-             last_name: orderInfo.billing.last_name,
-             addresses: [
-                 {
-                     company: orderInfo.billing.company,
-                     first_name: orderInfo.billing.first_name,
-                     last_name: orderInfo.billing.last_name,
-                     email: orderInfo.billing.email,
-                     phone: orderInfo.billing.phone,
-                     line1: orderInfo.billing.address_1,
-                     line2: orderInfo.billing.address_2,
-                     postal_code: orderInfo.billing.postcode,
-                     city: orderInfo.billing.city,
-                     country: orderInfo.billing.country,
-                 }
-             ],
-             email: orderInfo.billing.email,
-             phone: orderInfo.billing.phone,
-             birth_date: "1980-01-01",
-             birth_place: "Paris, France",
-             banking_data_collected: false, // ?
-             is_business: false,
-             business_id_number: "",
-             business_name: orderInfo.billing.company
-         }
-     });
- */
+    // switcher l'api api côté serveur next js => route api comme payplug
     const apiKey = process.env.NEXT_PUBLIC_ALMA_API_KEY; // 
     const payload = {
         payment: {
